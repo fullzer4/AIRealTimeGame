1ff520978ed3890f942936f9ed88946184ba92e3
fullzer4 <gabrielpelizzaro@gmail.com>
2024-12-19 22:12:36 -0300

    feat/k8s test

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   experimental: {
     reactCompiler: true,
   },
+  env: {
+    NODE_ENV: "production",
+  },
+  output: "export",
+  exportTrailingSlash: true,
+  productionBrowserSourceMaps: false,
 }
 
 export default nextConfig
